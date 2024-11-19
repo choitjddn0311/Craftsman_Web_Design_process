eed489c9bfed99b60dac1a82cb1aa3dbd4525426
choitjddn0311 <cswcjw311427@naver.com>
2024-11-19 18:20:41 +0900

    feat: class(2) menu and sub menu build and image slide build

---
+++ test/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    var i = 4;
+
+    setInterval(function(){
+        i--;
+        if(i==3) $(".b img").css("right","0"); // 여기서 방향 조정함 top left right bottom 등등...
+        $(".b img").eq(i).animate({"right":"1200px"},500);
+        if(i <= 1) i=4;
+    },2000);
+});
